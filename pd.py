092da9ad99cfb7e26e634e2ec70613a425e58c35
Daniel Helmrich <dahelmrich@gmail.com>
2021-02-23 17:13:39 +0100

    Add --latex-max-bold option

--- pd.py
+++ pd.py
@@ -17,12 +17,12 @@ parser.add_argument("--descending", help="Sort descending", action="store_true")
 parser.add_argument("--drop", help="Drop column(s)", nargs="+")
 parser.add_argument("--with-index", action="store_true", help="Specify for outputing the index column")
 parser.add_argument("--latex", action="store_true", help="Output latex table")
-parser.add_argument("--float-format", type=str, default=None, help="Specify the float format in the output, e.g. '%.4f'")
+parser.add_argument("--float-format", type=str, default="%.4f", help="Specify the float format in the output, e.g. '%%.4f'")
 parser.add_argument("--move", nargs="+", help="Moves column X right before after column Y; format: 'X::Y'", default=None)
 parser.add_argument("--rename", nargs="+", help="Renames columns, format: 'old name::new name'")
 parser.add_argument("--sep", default=",", help="Column seperator")
+parser.add_argument("--latex-max-bold", nargs="+", help="Marks the maximum value of each column in the latex output bold.", type=str, default=None)
 parsed = parser.parse_args()
-
 
 if parsed.input is None:
     text = ""
@@ -70,7 +70,7 @@ if parsed.transpose:
 
 if parsed.output is None:
     if parsed.pretty:
-        print(df)
+        print(df.to_string())
         exit(0)
     else:
         out_res = sys.stdout
@@ -79,6 +79,21 @@ else:
     out_res = parsed.output
 
 out_method = df.to_csv
+
+
 if parsed.latex:
-    out_method=df.to_latex
+    formatters_fns = {}
+    if parsed.latex_max_bold is not None:
+        frmt_cell = lambda x: parsed.float_format % x if isinstance(x, float) else str(x)
+        def make_max_formatter(max_value):
+            return lambda x: "\ŧextbf{%s}" % frmt_cell(x) if x == max_value else frmt_cell(x)
+        for col in parsed.latex_max_bold:
+            if col not in df.columns:
+                raise ValueError("Column %s not found" % col)
+            max_col_value = df[col].max()
+            formatters_fns[col] = make_max_formatter(max_col_value)
+        formatters = [formatters_fns[col] if col in formatters_fns else None for col in df.columns]
+    else:
+        formatters = None
+    out_method=lambda *args, **kwargs: df.to_latex(*args, formatters=formatters, escape=False, **kwargs)
 out_method(out_res, index= parsed.with_index, float_format=parsed.float_format)
