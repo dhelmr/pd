469c6de47cc6c192d6e79aff34e9e2b9c874db5d
Daniel Helmrich <dahelmrich@gmail.com>
2021-03-23 17:59:21 +0100

    Fix pipe problem and add json, html input/output format

--- pd.py
+++ pd.py
@@ -2,26 +2,48 @@
 
 import argparse
 import pandas
+import numpy as np
 import sys
 from io import StringIO
-    
+from enum import Enum
+
+from signal import signal, SIGPIPE, SIG_DFL
+signal(SIGPIPE,SIG_DFL) 
+
+class OutputFormats(Enum):
+    CSV = "csv"
+    LATEX = "latex"
+    JSON = "json"
+    HTML = "html"
+
+class InputFormats(Enum):
+    CSV = "csv"
+    JSON = "json"
+
+ALLOWED_JSON_ORIENTS = ["split", "records", "index", "columns", "values", "table"]
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--query", "-q", help="Pandas query")
 parser.add_argument("--input", "-i", help="Input file")
 parser.add_argument("--output", "-o", default=None, help="Output file, if not set the output is printed to stdout")
 parser.add_argument("--pretty", "-p", action="store_true")
+parser.add_argument("--pretty-short", "-pp", action="store_true")
 parser.add_argument("--transpose", "-t", action="store_true")
 parser.add_argument("--sort", "-s", nargs="+", help="Sort by column(s)")
 parser.add_argument("--descending", help="Sort descending", action="store_true")
 parser.add_argument("--drop", help="Drop column(s)", nargs="+")
 parser.add_argument("--with-index", action="store_true", help="Specify for outputing the index column")
-parser.add_argument("--latex", action="store_true", help="Output latex table")
+parser.add_argument("--to", help="Specify the output format. Choose from: %s" % [v.value for v in OutputFormats], type= lambda x: OutputFormats(x), default=OutputFormats.CSV)
+parser.add_argument("--from", dest="from_format", help="Specify the input format. Choose from: %s" % [v.value for v in InputFormats], type=lambda x: InputFormats(x), default=InputFormats.CSV)
 parser.add_argument("--float-format", type=str, default="%.4f", help="Specify the float format in the output, e.g. '%%.4f'")
 parser.add_argument("--move", nargs="+", help="Moves column X right before after column Y; format: 'X::Y'", default=None)
 parser.add_argument("--rename", nargs="+", help="Renames columns, format: 'old name::new name'")
 parser.add_argument("--sep", default=",", help="Column seperator")
 parser.add_argument("--latex-max-bold", nargs="+", help="Marks the maximum value of each column in the latex output bold.", type=str, default=None)
+parser.add_argument("--columns", action="store_true")
+parser.add_argument("--replace-nan", help="Replace all NaN values with another value", type=float)
+parser.add_argument("--json-out-orient", help="Specifies the json format. Choose one of %s. See https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.to_json.html?highlight=to_json#pandas.Series.to_json" % ALLOWED_JSON_ORIENTS, default="records", choices=ALLOWED_JSON_ORIENTS)
+parser.add_argument("--json-in-orient", help="Specifies the json orient. Choose one of %s. See https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.to_json.html?highlight=to_json#pandas.Series.to_json" % ALLOWED_JSON_ORIENTS, default="records", choices=ALLOWED_JSON_ORIENTS)
 parsed = parser.parse_args()
 
 if parsed.input is None:
@@ -33,7 +55,13 @@ if parsed.input is None:
 else:
     input_res = parsed.input
 
-df = pandas.read_csv(input_res, index_col=False, sep=parsed.sep)
+if parsed.from_format == InputFormats.CSV:
+    df = pandas.read_csv(input_res, index_col=False, sep=parsed.sep)
+elif parsed.from_format == InputFormats.JSON:
+    df = pandas.read_json(input_res, orient=parsed.json_in_orient)
+
+if parsed.replace_nan is not None:
+   df = df.replace(np.nan, parsed.replace_nan)
 
 if parsed.query is not None:
     df = df.query(parsed.query)
@@ -62,15 +90,21 @@ if parsed.move is not None:
         X,Y = operation.split("::")
         columns.insert(columns.index(Y), columns.pop(columns.index(X)))
     df = df.reindex(columns = columns)
-    
-
 
 if parsed.transpose:
     df = df.T
 
+if parsed.columns:
+    for col in df.columns:
+        print(col)
+    exit(0)
+
 if parsed.output is None:
     if parsed.pretty:
         print(df.to_string())
+        exit(0)
+    elif parsed.pretty_short:
+        print(df)
         exit(0)
     else:
         out_res = sys.stdout
@@ -78,22 +112,48 @@ if parsed.output is None:
 else:
     out_res = parsed.output
 
-out_method = df.to_csv
+def escape_latex(text: str) -> str:
+    return text.replace("_", "\\_")
 
+def wrap_latex_output(df, out_res, *args, replaces: dict, **kwargs):
+    text = df.to_latex(*args, **kwargs)
+    if replaces is not None:
+        for key, value in replaces.items():
+            text = text.replace(key, value)
+    if type(out_res) is str:
+        with open(out_res, "w") as f:
+            f.write(text)
+    else:
+        out_res.write(text)
 
-if parsed.latex:
+if parsed.to == OutputFormats.LATEX:
     formatters_fns = {}
     if parsed.latex_max_bold is not None:
+        replace_str_start = "QQA"
+        replace_str_end = "QQB"
         frmt_cell = lambda x: parsed.float_format % x if isinstance(x, float) else str(x)
         def make_max_formatter(max_value):
-            return lambda x: "\ŧextbf{%s}" % frmt_cell(x) if x == max_value else frmt_cell(x)
+            return lambda x: "%s%s%s" % (replace_str_start,frmt_cell(x), replace_str_end) if x == max_value else frmt_cell(x)
         for col in parsed.latex_max_bold:
             if col not in df.columns:
                 raise ValueError("Column %s not found" % col)
             max_col_value = df[col].max()
             formatters_fns[col] = make_max_formatter(max_col_value)
-        formatters = [formatters_fns[col] if col in formatters_fns else None for col in df.columns]
+        formatters = [formatters_fns[col] if col in formatters_fns else frmt_cell for col in df.columns]
+        replaces = {
+            replace_str_start: "\\textbf{",
+            replace_str_end: "}"
+        }
     else:
         formatters = None
-    out_method=lambda *args, **kwargs: df.to_latex(*args, formatters=formatters, escape=False, **kwargs)
-out_method(out_res, index= parsed.with_index, float_format=parsed.float_format)
+        replaces = None
+    out_method= lambda *args, **kwargs: wrap_latex_output(df, *args, formatters=formatters, replaces=replaces, float_format=parsed.float_format, index= parsed.with_index, **kwargs)
+elif parsed.to == OutputFormats.JSON:
+    out_method = lambda *args, **kwargs: df.to_json(*args, orient=parsed.json_out_orient)
+elif parsed.to == OutputFormats.HTML:
+     out_method = lambda *args, **kwargs: df.to_html(*args, float_format=parsed.float_format, index= parsed.with_index, **kwargs)
+elif parsed.to == OutputFormats.CSV:
+    out_method = lambda *args, **kwargs: df.to_csv(*args, float_format=parsed.float_format, index= parsed.with_index, **kwargs)
+else:
+    raise RuntimeError("Unexpected output format: %s" % parsed.to)
+out_method(out_res)
